10bd36ff2a3b0ac4b6755bd7a87f570744d3d4d8
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-07 00:10:25 +0800

    Migrate GET and POST functions to db for the following routes:
    -projects
    -roles
    -categories

--- api/server.js
+++ api/server.js
@@ -1,15 +1,17 @@
+// require('dotenv').load()
 const express = require('express')
+const Router = require('express-promise-router')
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const uuidv4 = require('uuid/v4')
 
+const db = require('../data/db')
+
 const app = express()
-const router = express.Router()
+const router = new Router()
 const token =
   'esfeyJ1c2VySWQiOiJiMDhmODZhZi0zNWRhLTQ4ZjItOGZhYi1jZWYzOTA0NUIhkufemQifQ';
-
-const port = 5000
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -28,158 +30,108 @@ const sendUserError = (msg, res) => {
 //   }
 // }
 
-let projects = [
-  {
-    id: 1,
-    name: 'Team Builder App',
-    pitch:
-      'Managing hundreds of student projects week in and week out for Build weeks gets a bit daunting! I need an app that helps me with product ideation and project curation.',
-    mvp: 'The product solves the problem',
-    stretch: 'Allow me to submit a CSV with the proper fields.',
-    roleAssignments: [
-      {
-        id: 1,
-        role: 'Web UI Developer',
-        assignedTo: 'Harry'
+// let projects = [
+//   {
+//     id: 1,
+//     name: 'Team Builder App',
+//     pitch:
+//       'Managing hundreds of student projects week in and week out for Build weeks gets a bit daunting! I need an app that helps me with product ideation and project curation.',
+//     mvp: 'The product solves the problem',
+//     stretch: 'Allow me to submit a CSV with the proper fields.',
+//     roleAssignments: [
+//       {
+//         id: 1,
+//         role: 'Web UI Developer',
+//         assignedTo: 'Harry'
 
-      },
-      {
-        id: 2,
-        role: 'Front-end Architect',
-        assignedTo: 'Ron'
+//       },
+//       {
+//         id: 2,
+//         role: 'Front-end Architect',
+//         assignedTo: 'Ron'
 
-      },
-      {
-        id: 3,
-        role: 'Back-end Architect',
-        assignedTo: 'Hermione'
-      }
-    ],
-    signUpList: [
-      {
-        id: 1,
-        role: 'UX Designer',
-        user: 'Greg'
-      },
-      {
-        id: 2,
-        role: 'Project Coordinator',
-        user: 'Alice'
-      },
-      {
-        id: 3,
-        role: 'Back-end Architect',
-        user: 'Sandra'
-      },
-    ],
-    category: 'Productivity',
-    projectComplete: false
-  },
-  {
-    id: 2,
-    name: 'Better Professor App',
-    pitch: `As a professor, I have a lot of grad students I mentor. I’d like to help keep them on track, but I can never remember their project deadlines, or when my feedback to them is due. I need an app that allows me to enter in all the important dates by category (ie student project deadline, feedback deadline, letter of recommendation deadline, etc.) and automatically remind me or the student before the deadline. Never miss an opportunity to member ever again!`,
-    mvp:
-      'As a user I want to login and see a list of students I mentor so I can add to or remove from the list. I also want to click on a student and see a list of their important projects or research papers I am mentoring on.  As a user I want to be able to create an automated messages to be sent to myself or a student I’m mentoring. A message should include a send date and time and a long text field that enables the message curator the ability to send a custom message.',
-    stretch: `(Mobile App) - Allow reminders to be pushed through the app. 
-        (Web App) - Allow for a social media sharing aspect that allows you to advertise the app to professors on linkedin.
-        To enable screen reader support, -To learn about keyboard shortcuts,`,
-    roleAssignments: [
-      {
-        id: 1,
-        role: 'UX Designer',
-        assignedTo: 'George'
-      },
-      {
-        id: 2,
-        role: 'iOS Developer',
-        assignedTo: 'Steve'
-      }
-    ],
-    signUpList: [
-      {
-        id: 1,
-        role: 'Project Coordinator',
-        user: 'Peter'
-      },
-      {
-        id: 2,
-        role: 'Back-end Architect',
-        user: 'Jen'
-      },
-      {
-        id: 3,
-        role: 'Web UI Developer',
-        user: 'Sandra'
-      },
-    ],
-    category: 'Education',
-    projectComplete: false
-  },
-  {
-    id: 3,
-    name: 'Bubl',
-    pitch:
-      'Imagine a social platform that helps students transition from elementary school to middle school. Bubl is a school specific social network and friendship connection tool.',
-    mvp:
-      'User can create an account, join an existing school account, and search for existing bubls to join. They can explore interests through hashtag searches. They can connect with others and share experiences.',
-    stretch: '',
-    roleAssignments: [],
-    signUpList: [],
-    category: 'Social Media',
-    projectComplete: true
-  }
-]
-
-let roles = [
-  {
-    id: 1,
-    name: 'Web UI Developer'
-  },
-  {
-    id: 2,
-    name: 'Front-end Architect'
-  },
-  {
-    id: 3,
-    name: 'Back-end Architect'
-  },
-  {
-    id: 4,
-    name: 'Project Support Coordinator'
-  },
-  {
-    id: 5,
-    name: 'UX Designer'
-  },
-]
-
-let categories = [
-  {
-    id: 1,
-    name: 'Productivity'
-  },
-  {
-    id: 2,
-    name: 'Education'
-  },
-  {
-    id: 3,
-    name: 'Social Impact'
-  },
-  {
-    id: 4,
-    name: 'Personal Finance'
-  },
-  {
-    id: 5,
-    name: 'Leisure and Entertainment'
-  },
-  {
-    id: 6,
-    name: 'Social Media'
-  }
-]
+//       },
+//       {
+//         id: 3,
+//         role: 'Back-end Architect',
+//         assignedTo: 'Hermione'
+//       }
+//     ],
+//     signUpList: [
+//       {
+//         id: 1,
+//         role: 'UX Designer',
+//         user: 'Greg'
+//       },
+//       {
+//         id: 2,
+//         role: 'Project Coordinator',
+//         user: 'Alice'
+//       },
+//       {
+//         id: 3,
+//         role: 'Back-end Architect',
+//         user: 'Sandra'
+//       },
+//     ],
+//     category: 'Productivity',
+//     projectComplete: false
+//   },
+//   {
+//     id: 2,
+//     name: 'Better Professor App',
+//     pitch: `As a professor, I have a lot of grad students I mentor. I’d like to help keep them on track, but I can never remember their project deadlines, or when my feedback to them is due. I need an app that allows me to enter in all the important dates by category (ie student project deadline, feedback deadline, letter of recommendation deadline, etc.) and automatically remind me or the student before the deadline. Never miss an opportunity to member ever again!`,
+//     mvp:
+//       'As a user I want to login and see a list of students I mentor so I can add to or remove from the list. I also want to click on a student and see a list of their important projects or research papers I am mentoring on.  As a user I want to be able to create an automated messages to be sent to myself or a student I’m mentoring. A message should include a send date and time and a long text field that enables the message curator the ability to send a custom message.',
+//     stretch: `(Mobile App) - Allow reminders to be pushed through the app. 
+//         (Web App) - Allow for a social media sharing aspect that allows you to advertise the app to professors on linkedin.
+//         To enable screen reader support, -To learn about keyboard shortcuts,`,
+//     roleAssignments: [
+//       {
+//         id: 1,
+//         role: 'UX Designer',
+//         assignedTo: 'George'
+//       },
+//       {
+//         id: 2,
+//         role: 'iOS Developer',
+//         assignedTo: 'Steve'
+//       }
+//     ],
+//     signUpList: [
+//       {
+//         id: 1,
+//         role: 'Project Coordinator',
+//         user: 'Peter'
+//       },
+//       {
+//         id: 2,
+//         role: 'Back-end Architect',
+//         user: 'Jen'
+//       },
+//       {
+//         id: 3,
+//         role: 'Web UI Developer',
+//         user: 'Sandra'
+//       },
+//     ],
+//     category: 'Education',
+//     projectComplete: false
+//   },
+//   {
+//     id: 3,
+//     name: 'Bubl',
+//     pitch:
+//       'Imagine a social platform that helps students transition from elementary school to middle school. Bubl is a school specific social network and friendship connection tool.',
+//     mvp:
+//       'User can create an account, join an existing school account, and search for existing bubls to join. They can explore interests through hashtag searches. They can connect with others and share experiences.',
+//     stretch: '',
+//     roleAssignments: [],
+//     signUpList: [],
+//     category: 'Social Media',
+//     projectComplete: true
+//   }
+// ]
 
 let users = [
   {
@@ -222,33 +174,65 @@ router.get('/', (req, res) => {
 })
 
 /*============== PROJECT ROUTES ==================*/
-router.get(`/projects`, (req, res) => {
-  res.json(projects)
-})
-
-router.get(`/projects/:id`, (req, res) => {
-  const { id } = req.params
-  console.log(`GET incoming ID: `, req.params.id)
-  const project = projects.find(p => p.id == id)
-  console.log(`GET Method /projects/:id `, project)
-
-  if (project) {
-    res.status(200).json(project)
-  } else {
-    console.log(`Project id is not there: `, project)
-    res.status(404).send({msg: `Project ${req.params.id} not found`})
+router.get(`/projects`, async (req, res) => {
+  try {
+    const { rows } = await db.query(`SELECT * FROM projects`)
+    res.send(rows)
+  } catch (ex) {
+    console.log(`Database query failed ${ex}`)
+    res.send(ex)
   }
+
 })
 
-router.post(`/projects`, (req, res) => {
-  // Create new project record
-  let newProject = { id: uuidv4(), ...req.body}
-  console.log(`New project created: `, newProject)
-  // Add new project to existing projet list
-  projects = [...projects, newProject]
-  console.log(`updated project list: `, projects)
-  // Send updated list 
-  res.send(projects)
+router.get(`/projects/:id`, async (req, res) => {
+  const { id } = req.params
+  try {
+    const { rows } = await db.query('SELECT * FROM projects WHERE id = $1', [id])
+    res.send(rows)
+  }
+  catch (ex) {
+    console.log(`Database query failed ${ex}`)
+    res.send(ex)
+  }
+
+  // console.log(`GET incoming ID: `, req.params.id)
+  // const project = projects.find(p => p.id == id)
+  // console.log(`GET Method /projects/:id `, project)
+
+  // if (project) {
+  //   res.status(200).json(project)
+  // } else {
+  //   console.log(`Project id is not there: `, project)
+  //   res.status(404).send({msg: `Project ${req.params.id} not found`})
+  // }
+})
+
+router.post(`/projects`, async (req, res) => {
+  try {
+    const { name, pitch, mvp, stretch, category, projectComplete } = req.body
+    await db.query(`INSERT INTO projects(name, pitch, mvp, stretch, 
+      category, projectComplete) VALUES($1, $2, $3, $4, $5, $6)`,
+      [name, pitch, mvp, stretch, category, projectComplete]
+    )
+    const { rows } = await db.query(`SELECT * FROM projects`)
+    res.send(rows)
+    // console.log(JSON.stringify(req.body))
+    // res.send(req.body)
+  } 
+  catch (ex) {
+    console.log(`Database query failed ${ex}`)
+    res.send(ex)
+  }
+  
+  // // Create new project record
+  // let newProject = { id: uuidv4(), ...req.body}
+  // console.log(`New project created: `, newProject)
+  // // Add new project to existing projet list
+  // projects = [...projects, newProject]
+  // console.log(`updated project list: `, projects)
+  // // Send updated list 
+  // res.send(projects)
 
 })
 
@@ -261,8 +245,8 @@ router.put(`/projects/:id`, (req, res) => {
     const updatedProject = { ...project, ...req.body }
     console.log(`updatedProject: `, updatedProject)
     projects = [...projects.map(project => {
-      if (project.id == id) {
-        return updatedProject
+      if (project.id == payload.id) {
+        return payload
       } else {
         return project
       }
@@ -344,63 +328,112 @@ router.delete(`/users/:id`, (req, res) => {
 
 
 /*============= ROLE ROUTES ===============*/
-router.get(`/roles`, (req, res) => {
-  res.json(roles)
-})
-
-router.get(`/roles/:id`, (req, res) => {
-  const { id } = req.params
-  console.log(`GET incoming ID: `, req.params.id)
-  const role = roles.find(p => p.id == id)
-  console.log(`GET Method /roles/:id `, role)
-
-  if (role) {
-    res.status(200).json(role)
-  } else {
-    console.log(`Role id is not there: `, role)
-    res.status(404).send({ msg: `Role ${req.params.id} not found` })
+router.get(`/roles`, async (req, res) => {
+  try {
+    const { rows } = await db.query(`SELECT * FROM roles`)
+    res.send(rows)
   }
+  catch (ex) {
+    console.log(`Database query error ${ex}`)
+    res.send(ex)
+  }
+
 })
 
-router.post(`/roles`, (req, res) => {
-  // Create new role record
-  let newRole = { id: uuidv4(), ...req.body }
-  console.log(`New role created: `, newRole)
-  // Add new role to existing projet list
-  roles = [...roles, newRole]
-  console.log(`updated role list: `, roles)
-  // Send updated list 
-  res.send(roles)
+router.get(`/roles/:id`, async (req, res) => {
+  const { id } = req.params
+  try {
+    const { rows } = await db.query('SELECT * FROM roles WHERE id = $1', [id])
+    res.send(rows)
+  }
+  catch (ex) {
+    console.log(`Database query error ${ex}`)
+    res.send(ex)
+  }
+
+  // console.log(`GET incoming ID: `, req.params.id)
+  // const role = roles.find(p => p.id == id)
+  // console.log(`GET Method /roles/:id `, role)
+
+  // if (role) {
+  //   res.status(200).json(role)
+  // } else {
+  //   console.log(`Role id is not there: `, role)
+  //   res.status(404).send({ msg: `Role ${req.params.id} not found` })
+  // }
 })
 
-router.put(`/roles/:id`, (req, res) => {
+router.post(`/roles`, async (req, res) => {
+  try {
+    const { name } = req.body
+    console.log(`incoming value: `, name)
+    await db.query(`INSERT INTO roles(name) VALUES ($1)`, [name])
+    const { rows } = await db.query(`SELECT * FROM roles`)
+    res.send(rows)
+  }
+  catch (ex) {
+    console.log(`Database query error ${ex}`)
+    res.send(ex)
+  }
+
+
+  // // Create new role record
+  // let newRole = { id: uuidv4(), ...req.body }
+  // console.log(`New role created: `, newRole)
+  // // Add new role to existing projet list
+  // roles = [...roles, newRole]
+  // console.log(`updated role list: `, roles)
+  // // Send updated list 
+  // res.send(roles)
+})
+
+router.put(`/roles/:id`, async (req, res) => {
   const { id } = req.params
   console.log(`PUT method invoked id: `, id)
-  const role = roles.find(role => role.id == id)
+  try {
 
-  if (role) {
-    const updatedRole = { ...role, ...req.body }
-    console.log(`updatedRole: `, updatedRole)
-    roles = [...roles.map(role => {
-      if (role.id == id) {
-        return updatedRole
-      } else {
-        return role
-      }
-    })]
-    console.log(`updated Role List: `, roles)
-    res.send(roles)
-  } else {
-    res.status(404).send({ msg: `role ${id} not found` })
+    const { rows } = await db.query(`SELECT * FROM projects`)
+    res.send(rows)
   }
+  catch (ex) {
+    console.log(`Database query error ${ex}`)
+    res.send(ex)
+  }
+
+  // const role = roles.find(role => role.id == id)
+
+  // if (role) {
+  //   const updatedRole = { ...role, ...req.body }
+  //   console.log(`updatedRole: `, updatedRole)
+  //   roles = [...roles.map(role => {
+  //     if (role.id == id) {
+  //       return updatedRole
+  //     } else {
+  //       return role
+  //     }
+  //   })]
+  //   console.log(`updated Role List: `, roles)
+  //   res.send(roles)
+  // } else {
+  //   res.status(404).send({ msg: `role ${id} not found` })
+  // }
 })
 
-router.delete(`/roles/:id`, (req, res) => {
+router.delete(`/roles/:id`, async (req, res) => {
   const { id } = req.params
   console.log(`Submit delete request: `, id)
-  roles = roles.filter(p => p.id !== Number(id))
+  try {
+    const { rows } = await db.query(`SELECT * FROM projects`)
+    res.send(rows)
+  }
+  catch (ex) {
+    console.log(`Database query error ${ex}`)
+    res.send(ex)
+  }
 
-  res.send(roles)
+  // roles = roles.filter(p => p.id !== Number(id))
+
+  // res.send(roles)
 
 })
 
@@ -423,15 +456,28 @@ router.get(`/categories/:id`, (req, res) => {
   }
 })
 
-router.post(`/categories`, (req, res) => {
-  // Create new categorie record
-  let newCategory = { id: uuidv4(), ...req.body }
-  console.log(`New categorie created: `, newCategory)
-  // Add new categorie to existing projet list
-  categories = [...categories, newCategory]
-  console.log(`updated categories: `, categories)
-  // Send updated list 
-  res.send(categories)
+router.post(`/categories`, async (req, res) => {
+  const { name } = req.body
+  try {
+    await db.query(`INSERT INTO categories(name) VALUES ($1)`, [name])
+    const { rows } = await db.query(`SELECT * FROM categories`)
+    res.send(rows)
+  }
+  catch (ex) {
+    console.log(`Database query error ${ex}`)
+    res.send(ex)
+  }
+
+
+
+  // // Create new categorie record
+  // let newCategory = { id: uuidv4(), ...req.body }
+  // console.log(`New categorie created: `, newCategory)
+  // // Add new categorie to existing projet list
+  // categories = [...categories, newCategory]
+  // console.log(`updated categories: `, categories)
+  // // Send updated list 
+  // res.send(categories)
 })
 
 router.put(`/categories/:id`, (req, res) => {
