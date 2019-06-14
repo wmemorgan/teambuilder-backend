2d869727adba3b570f9ef3f5bb045948de717d7c
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-15 01:31:19 +0800

    Move to environment variables

--- data/db.js
+++ data/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 // Database connection pooling
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: 'lambda_dev',
-  password: 'i<3Lambd4',
-  host: 'devdb.ci2w4mxsfhhy.us-east-1.rds.amazonaws.com',
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PWD,
+  host: process.env.DATABASE_HOST,
   port: 5432,
   database: 'thursdaydb'
 })
